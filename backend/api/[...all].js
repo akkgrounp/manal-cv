05b960e249708ba5ffc7794b6dee1ba2024f455c
Manal <manalk14322@gmail.com>
2026-04-02 00:14:32 +0500

    Humanize KK website repo and remove legacy demo app

--- backend/api/[...all].js
+++
@@ -1,7 +0,0 @@
-const dotenv = require("dotenv");
-const app = require("../app");
-
-dotenv.config();
-
-module.exports = app;
-
--- backend/utils/inMemoryStore.js
+++
@@ -1,7 +0,0 @@
-// Lightweight in-memory store used when MongoDB is unavailable.
-const store = {
-  users: [],
-  resumes: [],
-};
-
-module.exports = store;
